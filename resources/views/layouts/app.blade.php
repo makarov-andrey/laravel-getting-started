1c07ee45ac67e4cc4e4e71ec7f5551db9a549512
Makarov Andrey <mr.wertmax@gmail.com>
2017-08-23 18:28:25 +0400

    initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Laravel Quickstart - Basic</title>
+
+    <link rel="stylesheet" href="/css/app.css">
+    <script src="/js/app.js"></script>
+</head>
+
+<body>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <!-- Содержимое Navbar -->
+    </nav>
+</div>
+
+@yield('content')
+</body>
+</html>
